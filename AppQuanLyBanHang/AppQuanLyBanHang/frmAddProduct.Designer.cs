413276798b89a814cb6aea097e20376b86e1ae6e
ttnhac <ttnhac.official@gmail.com>
2024-08-06 12:53:51 +0700

    1

---
+++ AppQuanLyBanHang/AppQuanLyBanHang/frmAddProduct.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace AppQuanLyBanHang
+{
+    partial class frmAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ProductName = new System.Windows.Forms.TextBox();
+            this.cbo_Brand = new System.Windows.Forms.ComboBox();
+            this.btn_Luu = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbo_Category = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Gia = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_GiaDiscount = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_Avatar = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Soluong = new System.Windows.Forms.TextBox();
+            this.txt_shortdes = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_fulldes = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_browseImg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 140);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Loại sản phẩm";
+            // 
+            // txt_ProductName
+            // 
+            this.txt_ProductName.Location = new System.Drawing.Point(148, 30);
+            this.txt_ProductName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_ProductName.Name = "txt_ProductName";
+            this.txt_ProductName.Size = new System.Drawing.Size(160, 22);
+            this.txt_ProductName.TabIndex = 1;
+            // 
+            // cbo_Brand
+            // 
+            this.cbo_Brand.FormattingEnabled = true;
+            this.cbo_Brand.Location = new System.Drawing.Point(148, 84);
+            this.cbo_Brand.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbo_Brand.Name = "cbo_Brand";
+            this.cbo_Brand.Size = new System.Drawing.Size(160, 24);
+            this.cbo_Brand.TabIndex = 2;
+            // 
+            // btn_Luu
+            // 
+            this.btn_Luu.Location = new System.Drawing.Point(160, 406);
+            this.btn_Luu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Luu.Name = "btn_Luu";
+            this.btn_Luu.Size = new System.Drawing.Size(149, 47);
+            this.btn_Luu.TabIndex = 3;
+            this.btn_Luu.Text = "Lưu";
+            this.btn_Luu.UseVisualStyleBackColor = true;
+            this.btn_Luu.Click += new System.EventHandler(this.btn_Luu_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 87);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Thương hiệu";
+            // 
+            // cbo_Category
+            // 
+            this.cbo_Category.FormattingEnabled = true;
+            this.cbo_Category.Location = new System.Drawing.Point(148, 137);
+            this.cbo_Category.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbo_Category.Name = "cbo_Category";
+            this.cbo_Category.Size = new System.Drawing.Size(160, 24);
+            this.cbo_Category.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 33);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Tên sản phẩm";
+            // 
+            // txt_Gia
+            // 
+            this.txt_Gia.Location = new System.Drawing.Point(148, 190);
+            this.txt_Gia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Gia.Name = "txt_Gia";
+            this.txt_Gia.Size = new System.Drawing.Size(160, 22);
+            this.txt_Gia.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 193);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Giá";
+            // 
+            // txt_GiaDiscount
+            // 
+            this.txt_GiaDiscount.Location = new System.Drawing.Point(148, 242);
+            this.txt_GiaDiscount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_GiaDiscount.Name = "txt_GiaDiscount";
+            this.txt_GiaDiscount.Size = new System.Drawing.Size(160, 22);
+            this.txt_GiaDiscount.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 246);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Giá discount";
+            // 
+            // txt_Avatar
+            // 
+            this.txt_Avatar.Location = new System.Drawing.Point(148, 345);
+            this.txt_Avatar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Avatar.Name = "txt_Avatar";
+            this.txt_Avatar.Size = new System.Drawing.Size(160, 22);
+            this.txt_Avatar.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(40, 348);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 17);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Avatar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 295);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Số lượng";
+            // 
+            // txt_Soluong
+            // 
+            this.txt_Soluong.Location = new System.Drawing.Point(148, 292);
+            this.txt_Soluong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Soluong.Name = "txt_Soluong";
+            this.txt_Soluong.Size = new System.Drawing.Size(160, 22);
+            this.txt_Soluong.TabIndex = 11;
+            // 
+            // txt_shortdes
+            // 
+            this.txt_shortdes.Location = new System.Drawing.Point(395, 50);
+            this.txt_shortdes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_shortdes.Name = "txt_shortdes";
+            this.txt_shortdes.Size = new System.Drawing.Size(287, 58);
+            this.txt_shortdes.TabIndex = 18;
+            this.txt_shortdes.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(391, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Mô tả ngắn";
+            // 
+            // txt_fulldes
+            // 
+            this.txt_fulldes.Location = new System.Drawing.Point(395, 160);
+            this.txt_fulldes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_fulldes.Name = "txt_fulldes";
+            this.txt_fulldes.Size = new System.Drawing.Size(287, 63);
+            this.txt_fulldes.TabIndex = 18;
+            this.txt_fulldes.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 140);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Mô tả dài";
+            // 
+            // btn_browseImg
+            // 
+            this.btn_browseImg.Location = new System.Drawing.Point(317, 342);
+            this.btn_browseImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_browseImg.Name = "btn_browseImg";
+            this.btn_browseImg.Size = new System.Drawing.Size(100, 28);
+            this.btn_browseImg.TabIndex = 20;
+            this.btn_browseImg.Text = "Browse";
+            this.btn_browseImg.UseVisualStyleBackColor = true;
+            this.btn_browseImg.Click += new System.EventHandler(this.btn_browseImg_Click);
+            // 
+            // frmAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 468);
+            this.Controls.Add(this.btn_browseImg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_fulldes);
+            this.Controls.Add(this.txt_shortdes);
+            this.Controls.Add(this.txt_Avatar);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_Soluong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_GiaDiscount);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_Gia);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn_Luu);
+            this.Controls.Add(this.cbo_Category);
+            this.Controls.Add(this.cbo_Brand);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_ProductName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmAddProduct";
+            this.Text = "frmAddProduct";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmAddProduct_FormClosed);
+            this.Load += new System.EventHandler(this.frmAddProduct_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ProductName;
+        private System.Windows.Forms.ComboBox cbo_Brand;
+        private System.Windows.Forms.Button btn_Luu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbo_Category;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Gia;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_GiaDiscount;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_Avatar;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Soluong;
+        private System.Windows.Forms.RichTextBox txt_shortdes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txt_fulldes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_browseImg;
+    }
+}
